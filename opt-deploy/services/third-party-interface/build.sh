4786ea5c87324f4d39648d0ca6e14c5d5856eab4
hyteer <hyteer@qq.com>
2017-05-06 01:47:24 +0800

    thrid debug

--- opt-deploy/services/third-party-interface/build.sh
+++ opt-deploy/services/third-party-interface/build.sh
@@ -8,17 +8,8 @@ CODE="${WORKDIR}/_code_/${SRV_NAME}"
 # thrift gen
 cd ${CODE}
 echo "===生成thrift code..."
-    for file in ./thriftFiles/*.thrift
-    do
-        if test -f ${file}
-        then
-        	file2=`echo ${file}|cut -c 3-`
-            docker run -v "$PWD:/data" thrift thrift -o /data --gen php:server /data/${file2}
-        else
-            echo "${file} is not a file..."
-        fi
-    done
-cp -rf gen-php/thriftServices/* thriftServices/
+chmod +x thrift-file-generate.sh
+./thrift-file-generate.sh
 echo "===生成结果..."
 ls thriftServices
 
